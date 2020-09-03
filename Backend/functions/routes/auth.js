5c1deaee3432aa6bb8814a248a2107ba2cea1aad
Chathura Nilanka <chathuranilanka1024@gmail.com>
2020-09-03 11:35:35 +0530

    Adding CRUD functions

--- Backend/functions/routes/auth.js
+++ Backend/functions/routes/auth.js
@@ -30,7 +30,7 @@ router.get('/profile', UserAuth, async (request, response) => {
         });
     }
 
-})
+});
 
 router.put('/profile', UserAuth, async (request, response) => {
 
@@ -67,5 +67,108 @@ router.put('/profile', UserAuth, async (request, response) => {
     })
 });
 
+router.post('/records', UserAuth, async (request, response) => {
+    const db = admin.firestore();
+    let userId = request.user.userId;
+    const recordObj = {
+        uid: userId,
+        title: request.body.title,
+        description: request.body.description,
+        date: request.body.date,
+        createdAt: new Date().toISOString()
+    };
+    var recordsRef = db.collection('records');
+    return Promise.all([
+        recordsRef.doc().set(recordObj)
+    ]).then((doc) => {
+        return response.status(201).json({
+            message: `New Record Item added successfully`
+        });
+    }).catch((err) => {
+        console.error(err);
+        return response.status(500).json({
+            message: `Something went wrong`
+        });
+
+    })
+});
+
+
+router.get('/records', UserAuth, async (request, response) => {
+    const db = admin.firestore();
+    try {
+        let userId = request.user.userId;
+        let userrecordItems = [];
+        await db.collection("records").where("uid", '==', userId).get()
+            .then(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                    let record = Object.assign({ id: doc.id }, doc.data())
+                    console.log(doc.id);
+                    userrecordItems.push(record);
+                });
+            });
+
+        return response.status(200).json({
+            userrecordItems
+        })
+
+
+    } catch (error) {
+        console.log(error)
+        return response.status(500).json({
+            message: `Something went wrong`
+        });
+    }
+
+});
+
+router.delete('/records/:recordId', UserAuth, async (request, response) => {
+    const db = admin.firestore();
+    let recordId = request.params.recordId;
+    console.log(recordId)
+    try {
+        let userId = request.user.userId;
+        const recordsRef = db.collection('records');
+        const record = await recordsRef.doc(recordId).delete();
+        
+        return response.status(200).json({
+            message: `Deleted successfully`
+        });
+
+    } catch (error) {
+        console.log(error)
+        return response.status(500).json({
+            message: `Something went wrong`
+        });
+    }
+
+});
+
+router.put('/records/:recordId', UserAuth, async (request, response) => {
+    const db = admin.firestore();
+    let recordId = request.params.recordId;
+    const recordObj = {
+        title: request.body.title,
+        description: request.body.description,
+        date: request.body.date,
+        updatedAt: new Date().toISOString()
+    };
+    console.log(recordId)
+    try {
+        const recordsRef = db.collection('records');
+        const record = await recordsRef.doc(recordId).update(recordObj);
+        
+        return response.status(200).json({
+            message: `Updated successfully`
+        });
+
+    } catch (error) {
+        console.log(error)
+        return response.status(500).json({
+            message: `Something went wrong`
+        });
+    }
+
+});
 
 module.exports = router;
